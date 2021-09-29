880d04a2fe8ec3222c4eded8c3786d468b91aff6
Dmitry <d>
2021-09-29 18:47:17 +0300

    home

--- src/main/java/reader/ReadFromFile.java
+++ src/main/java/reader/ReadFromFile.java
@@ -1,5 +1,26 @@
 package reader;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class ReadFromFile {
+
+    public List<Integer> read(File file) throws IOException {
+        List<Integer> list = new ArrayList<>();
+        try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                String[] strings = line.split(" ");
+                for (String str : strings) {
+                    list.add(Integer.parseInt(str));
+                }
+            }
+        }
+        return list;
+    }
 
 }
