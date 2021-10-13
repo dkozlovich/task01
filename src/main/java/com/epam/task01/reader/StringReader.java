7d3cc1e1cbac4dc72338df2ddb9fd3eadd99999e
Dmitry <d>
2021-10-13 21:07:36 +0300

    home

--- src/main/java/com/epam/task01/reader/StringReader.java
+++ src/main/java/com/epam/task01/reader/StringReader.java
@@ -5,5 +5,5 @@ import com.epam.task01.exception.CustomArrayException;
 import java.io.File;
 
 public interface StringReader<T> {
-    String read(File file) throws CustomArrayException;
+    String read(T t) throws CustomArrayException;
 }
